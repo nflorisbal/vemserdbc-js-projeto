770fe7e54dc6930edfc999618c2e00a5de0fa331
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-18 17:21:25 -0300

    add classes Candidato, Vagas e Candidaturas

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,8 +7,42 @@ class Usuario {
     senha;
     candidaturas = []; // lista de Candidatura
 
+    constructor(id, tipo, nome, dataNascimento, email, senha) {
+        this.id = id;
+        this.tipo = tipo;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.senha = senha;
+    }
 }
 
+// recebe as candidaturas do usuario e a vaga para qual se candidatou
+class Candidatura {
+    idVaga;
+    idCandidato;
+    reprovado = false; 
+
+    constructor(idVaga, idCandidato) {
+        this.idVaga = idVaga;
+        this.idCandidato = idCandidato;
+    }
+}
+
+class Vaga {
+    id; //(automático json-server)
+    titulo;
+    descricao;
+    remuneracao; //(salvar no formato: R$ 3.200,50)
+    candidatos = []; // lista de Trabalhadores candidatados na vaga
+
+    constructor(id, titulo, descricao, remuneracao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.remuneracao = remuneracao;
+    }
+}
 
 const alternarClasses = (elemento, ...classes) => {
     classes.forEach(classe => {
