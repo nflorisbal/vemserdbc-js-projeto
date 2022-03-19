52ab60433bf57e1273f0770f3caf32bcc171d675
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-19 18:17:17 -0300

    add função detalharVaga

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,7 @@
 
 const USUARIOS_URL = 'http://localhost:3000/usuarios';
 const VAGAS_URL = 'http://localhost:3000/vagas';
-const CLASS_LI_VAGA = [ 'd-flex', 'justify-content-between', 'border', 'border-dark', 'rounded', 'p-3', 'w-100', 'mb-3' ];
+const CLASS_LI_VAGA = ['d-flex', 'justify-content-between', 'border', 'border-dark', 'rounded', 'p-3', 'w-100', 'mb-3'];
 
 let permissaoUsuario;
 
@@ -28,7 +28,7 @@ class Candidatura {
     // recebe as candidaturas do usuario e a vaga para qual se candidatou
     idVaga;
     idCandidato;
-    reprovado = false; 
+    reprovado = false;
 
     constructor(idVaga, idCandidato) {
         this.idVaga = idVaga;
@@ -54,8 +54,8 @@ class Vaga {
 //#region Funções Utilitárias
 
 const adicionarAtributos = (elemento, id, classes) => {
-	elemento.setAttribute('id', id);
-	if(classes !== undefined) elemento.classList.add(...classes);
+    elemento.setAttribute('id', id);
+    if (classes !== undefined) elemento.classList.add(...classes);
 }
 
 const alternarClasses = (elemento, ...classes) => {
@@ -77,40 +77,40 @@ const esqueceuSenha = async () => {
 
     try {
         const resolve = await axios.get(USUARIOS_URL);
-        
+
         const usuario = resolve.data.find(e => e.email === email.value);
 
-        if(usuario === undefined)
+        if (usuario === undefined)
             alert('E-mail do usuário não localizado.')
         else
             alert(`Senha recuperada!\n\nUsuário: ${usuario.email}\nSenha: ${usuario.senha}`);
 
-    } catch(erro) {
+    } catch (erro) {
         console.log(`Ocorreu algum erro durante a recuperação da senha. (${erro})`);
     }
 }
 
 const limparCampos = (...campos) => {
-	campos.forEach(e => {
+    campos.forEach(e => {
         e.value = '';
     })
 }
 
 const primeiraLetra = string => {
-	array = string.split(' ');
+    array = string.split(' ');
 
-	for (i in array) {
-		array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
-	}
+    for (i in array) {
+        array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+    }
 
-	return array.join(' ');
+    return array.join(' ');
 }
 
 const permissoesUsuario = permissao => {
     const botoesCadastroVagas = document.getElementById('job-btns');
     const botaoSairCadastroVagas = document.getElementById('exit-btn');
-    
-    switch(permissao) {        
+
+    switch (permissao) {
         case '1':
             botaoSairCadastroVagas.classList.add('btn-dark');
             botoesCadastroVagas.classList.remove('justify-content-between');
@@ -132,58 +132,58 @@ const permissoesUsuario = permissao => {
 
 //#region Validação Nome
 const validarNome = () => {
-	let nomeInput = document.getElementById('nome-input-registration');
-	let nome = nomeInput.value;
+    let nomeInput = document.getElementById('nome-input-registration');
+    let nome = nomeInput.value;
 
-	let possuiSohLetras = [...nome].every(letra => letra.toLowerCase() !== letra.toUpperCase() || letra == ' ');
+    let possuiSohLetras = [...nome].every(letra => letra.toLowerCase() !== letra.toUpperCase() || letra == ' ');
     let naoEhVazio = nome !== '';
 
-	let ehValido = possuiSohLetras && naoEhVazio;
+    let ehValido = possuiSohLetras && naoEhVazio;
 
-	// para setar o texto de erro em vermelho
-	let erroNome = document.getElementById('nome-registration-error');
-	erroNome.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
+    // para setar o texto de erro em vermelho
+    let erroNome = document.getElementById('nome-registration-error');
+    erroNome.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
 
-	return ehValido;
+    return ehValido;
 }
 
 //#endregion Validação Nome
 
 //#region Validação Data
-const validarData = () => { 
+const validarData = () => {
     let inputData = document.getElementById('date-input-registration');
     let dataDigitada = inputData.value;
-  
+
     adicionarMascaraData(inputData, dataDigitada);
-  
+
     let dataConvertida = moment(dataDigitada, 'DDMMYYYY');
-  
+
     let dezoitoAnosAtras = moment().diff(dataConvertida, 'years') >= 18;
-  
+
     // comparações de data - date1.isBefore(date2)  /  date1.isAfter(date2)  /  date1.isSameOrBefore(date2)  /  date1.isSameOrAfter(date2)
     let dataAnteriorHoje = dataConvertida.isBefore(moment());
-  
+
     let ehValido = dataConvertida.isValid() && dataAnteriorHoje && dezoitoAnosAtras && dataDigitada.length === 10; // 10/05/2001
-  
+
     // para setar o texto de erro em vermelho
     let erroData = document.getElementById('date-registration-error');
     erroData.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
-  
+
     return ehValido;
-  }
-  
-  const adicionarMascaraData = (input, data) => {
+}
+
+const adicionarMascaraData = (input, data) => {
     let listaCaracteres = [...data];
-    
+
     let listaFiltrada = listaCaracteres.filter(c => !isNaN(parseInt(c)));
-    if(listaFiltrada && listaFiltrada.length) {
+    if (listaFiltrada && listaFiltrada.length) {
         let dataDigitada = listaFiltrada.join('');
-  
+
         const { length } = dataDigitada;
-  
-        switch(length) { 
+
+        switch (length) {
             case 0: case 1: case 2:
-                input.value = dataDigitada; 
+                input.value = dataDigitada;
                 break;
             case 3: case 4:
                 input.value = `${dataDigitada.substring(0, 2)}/${dataDigitada.substring(2, 4)}`;
@@ -192,93 +192,93 @@ const validarData = () => {
                 input.value = `${dataDigitada.substring(0, 2)}/${dataDigitada.substring(2, 4)}/${dataDigitada.substring(4, 8)}`;
         }
     }
-  }
-  //#endregion Validação Data
+}
+//#endregion Validação Data
 
 //#region Validação Email
 const validarEmail = () => {
     let emailDigitado = document.getElementById('email-input-registration').value;
     let listaCaracteres = emailDigitado.split(''); // [...emailDigitado]
-  
+
     let emailSplit = emailDigitado.split('@');
-    
+
     let possuiArroba = emailSplit.length > 1;
-  
+
     let dominioEmail = possuiArroba ? emailSplit[1] : '';
     let dominioEmailSplit = dominioEmail.split('.');
-  
+
     let possuiPontosNoDominio = dominioEmailSplit.length > 1;
-  
-    let possuiCaracteresEntrePontos = dominioEmailSplit.every( d => d.length > 1 );
-  
+
+    let possuiCaracteresEntrePontos = dominioEmailSplit.every(d => d.length > 1);
+
     let comecaComLetra = listaCaracteres.length ? listaCaracteres[0].toUpperCase() !== listaCaracteres[0].toLowerCase() : false;
-  
+
     let ehValido = possuiArroba && possuiPontosNoDominio && possuiCaracteresEntrePontos && comecaComLetra;
-  
+
     // para setar o texto de erro em vermelho
     let erroEmail = document.getElementById('email-registration-error');
     erroEmail.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
-  
+
     return ehValido;
-  }
-  //#endregion Validação Email
+}
+//#endregion Validação Email
 
 //#region Validação Senha
 const validarSenha = () => {
     let senhaDigitada = document.getElementById('password-input-registration').value;
     let listaCaracteres = senhaDigitada.split('');
-  
-    let letras = listaCaracteres.filter( char => char.toLowerCase() !== char.toUpperCase() );
-  
-    let possuiLetraMaiuscula = letras.some( l => l.toUpperCase() === l ); // "A".toUppercase() === "A"
-    let possuiLetraMinuscula = letras.some( l => l.toLowerCase() === l );
-  
-    let possuiCharEspecial = listaCaracteres.some( char => char.toLowerCase() === char.toUpperCase() && isNaN(parseInt(char)) );
-    let possuiNumero = listaCaracteres.some( char => char.toLowerCase() === char.toUpperCase() && !isNaN(parseInt(char)) );
-  
+
+    let letras = listaCaracteres.filter(char => char.toLowerCase() !== char.toUpperCase());
+
+    let possuiLetraMaiuscula = letras.some(l => l.toUpperCase() === l); // "A".toUppercase() === "A"
+    let possuiLetraMinuscula = letras.some(l => l.toLowerCase() === l);
+
+    let possuiCharEspecial = listaCaracteres.some(char => char.toLowerCase() === char.toUpperCase() && isNaN(parseInt(char)));
+    let possuiNumero = listaCaracteres.some(char => char.toLowerCase() === char.toUpperCase() && !isNaN(parseInt(char)));
+
     let possuiOitoCaracteres = senhaDigitada.length >= 8;
-  
+
     let naoPossuiEspacos = !senhaDigitada.includes(' ');
-  
-    let ehValido = possuiOitoCaracteres && possuiLetraMaiuscula && possuiLetraMinuscula && 
+
+    let ehValido = possuiOitoCaracteres && possuiLetraMaiuscula && possuiLetraMinuscula &&
         possuiCharEspecial && possuiNumero && naoPossuiEspacos;
-  
+
     // para setar o texto de erro em vermelho
     let erroSenha = document.getElementById('password-registration-error');
     erroSenha.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
-  
+
     return ehValido;
-  }
-  //#endregion Validação Senha
+}
+//#endregion Validação Senha
 
 //#endregion #region Validação Inputs Usuário
 
 //#region Validação Inputs Vaga
 
 const validarTitulo = () => {
-	const tituloInput = document.getElementById('title-input-registration');
-	const titulo = tituloInput.value;
+    const tituloInput = document.getElementById('title-input-registration');
+    const titulo = tituloInput.value;
 
-	const ehValido = titulo !== '';
+    const ehValido = titulo !== '';
 
-	// para setar o texto de erro em vermelho
-	let erroTitulo = document.getElementById('title-registration-error');
-	erroTitulo.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
+    // para setar o texto de erro em vermelho
+    let erroTitulo = document.getElementById('title-registration-error');
+    erroTitulo.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
 
-	return ehValido;
+    return ehValido;
 }
 
 const validarDescricao = () => {
-	const descricaoTextarea = document.getElementById('description-textarea-registration');
-	const descricao = descricaoTextarea.value;
+    const descricaoTextarea = document.getElementById('description-textarea-registration');
+    const descricao = descricaoTextarea.value;
 
-	const ehValido = descricao !== '';
+    const ehValido = descricao !== '';
 
-	// para setar o texto de erro em vermelho
-	let erroDescricao = document.getElementById('description-registration-error');
-	erroDescricao.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
+    // para setar o texto de erro em vermelho
+    let erroDescricao = document.getElementById('description-registration-error');
+    erroDescricao.setAttribute('class', ehValido ? 'd-none' : 'text-danger');
 
-	return ehValido;
+    return ehValido;
 }
 
 const validarRemuneracao = () => {
@@ -290,44 +290,44 @@ const validarRemuneracao = () => {
 
 const validarCadastroUsuario = (event) => {
     event.preventDefault();
-	let cadastroValido = validarNome() && validarData() && validarEmail() && validarSenha();
+    let cadastroValido = validarNome() && validarData() && validarEmail() && validarSenha();
 
-	if (cadastroValido) {
-		cadastrarUsuario(event);
-	}
+    if (cadastroValido) {
+        cadastrarUsuario(event);
+    }
 }
 
 const cadastrarUsuario = (event) => {
-	event.preventDefault();
+    event.preventDefault();
 
     let tipo = document.getElementById('lista-tipo-usuario');
-	let nome = document.getElementById('nome-input-registration');
-	let dataNascimento = document.getElementById('date-input-registration');
-	let email = document.getElementById('email-input-registration');
-	let senha = document.getElementById('password-input-registration');
+    let nome = document.getElementById('nome-input-registration');
+    let dataNascimento = document.getElementById('date-input-registration');
+    let email = document.getElementById('email-input-registration');
+    let senha = document.getElementById('password-input-registration');
 
-	nome.value = primeiraLetra(nome.value);
+    nome.value = primeiraLetra(nome.value);
 
     const usuario = new Usuario(tipo.value, nome.value, dataNascimento.value, email.value, senha.value);
 
-	axios.post(USUARIOS_URL, usuario)
-		.then((resolve) => {
-			console.log(resolve.data);
-			irPara('registration', 'login');
-		}, (reject) => {
-			console.log(`Ocorreu algum erro durante o cadastro do usuário. (${reject})`);
-		});
+    axios.post(USUARIOS_URL, usuario)
+        .then((resolve) => {
+            console.log(resolve.data);
+            irPara('registration', 'login');
+        }, (reject) => {
+            console.log(`Ocorreu algum erro durante o cadastro do usuário. (${reject})`);
+        });
 
     limparCampos(tipo, nome, dataNascimento, email, senha);
 }
 
 const validarCadastroVaga = (event) => {
     event.preventDefault();
-	let cadastroValido = validarTitulo() && validarDescricao() && validarRemuneracao();
+    let cadastroValido = validarTitulo() && validarDescricao() && validarRemuneracao();
 
-	if (cadastroValido) {
-		cadastrarVaga(event);
-	}
+    if (cadastroValido) {
+        cadastrarVaga(event);
+    }
 }
 
 const cadastrarVaga = (event) => {
@@ -340,12 +340,12 @@ const cadastrarVaga = (event) => {
     const vaga = new Vaga(titulo.value, descricao.value, remuneracao.value);
 
     axios.post(VAGAS_URL, vaga)
-		.then((resolve) => {
+        .then((resolve) => {
             console.log(resolve.data);
-            
+
             const ul = document.getElementById('ul-vagas');
             const li = document.createElement('li');
-            
+
             adicionarAtributos(li, `li-vaga-${resolve.data.id}`, CLASS_LI_VAGA);
             ul.appendChild(li);
 
@@ -354,27 +354,28 @@ const cadastrarVaga = (event) => {
             spanTitulo.textContent = `Vaga: ${resolve.data.titulo}`;
             spanRemuneracao.textContent = `Remuneração: ${resolve.data.remuneracao}`;
             li.append(spanTitulo, spanRemuneracao);
+            li.addEventListener('click', detalharVaga);
 
             alert('Vaga cadastrada com sucesso!');
 
-			irPara('jobs-registration', 'list-jobs');
-		}, (reject) => {
-			console.log(`Ocorreu algum erro durante o cadastro da vaga. (${reject})`);
-		});
-    
+            irPara('jobs-registration', 'list-jobs');
+        }, (reject) => {
+            console.log(`Ocorreu algum erro durante o cadastro da vaga. (${reject})`);
+        });
+
     limparCampos(titulo, descricao, remuneracao);
 }
 
 const validarLogin = async () => {
     const email = document.getElementById('email-input-login');
-	const senha = document.getElementById('password-input-login');
+    const senha = document.getElementById('password-input-login');
     const erro = document.getElementById('login-error');
 
-    try{
+    try {
         const resolve = await axios.get(USUARIOS_URL);
-            
+
         usuario = resolve.data.find(e => e.email === email.value && e.senha === senha.value);
-        // debugger;
+  
         if (usuario === undefined)
             erro.classList.remove('d-none');
         else {
@@ -385,9 +386,9 @@ const validarLogin = async () => {
             buscarVagas();
             irPara('login', 'list-jobs');
         }
-    } catch(erro) {
+    } catch (erro) {
         console.log(`Ocorreu alguma erro durante o login. (${erro})`);
-    }   
+    }
 }
 
 const buscarVagas = async () => {
@@ -409,14 +410,41 @@ const buscarVagas = async () => {
             spanTitulo.textContent = `Vaga: ${e.titulo}`;
             spanRemuneracao.textContent = `Remuneração: ${e.remuneracao}`;
             li.append(spanTitulo, spanRemuneracao);
+            li.addEventListener('click', detalharVaga);
         })
-
     } catch (erro) {
         console.log(`Ocorreu alguma erro a busca das vagas. (${erro})`);
     }
 }
 
-const limparVagas = () =>{
+const limparVagas = (element) => {
     const ul = document.getElementById('ul-vagas');
     ul.remove();
+}
+
+const detalharVaga = async (event) => {
+    const id = event.target.id;
+    const idSplit = id.split('-');
+    const idElemento = idSplit[idSplit.length - 1];
+
+    try {
+        const resolve = await axios.get(VAGAS_URL);
+        const vaga = resolve.data.find(e => e.id === parseInt(idElemento));
+
+        const div = document.getElementById('jobs-content-description');
+        const pTitulo = document.createElement('p');
+        const pDescricao = document.createElement('p');
+        const pRemuneracao = document.createElement('p');
+
+        pTitulo.textContent = `Titulo: ${vaga.titulo}`;
+        pDescricao.textContent = `Descrição: ${vaga.descricao}`;
+        pRemuneracao.textContent = `Remuneração: ${vaga.remuneracao}`;
+
+        div.append(pTitulo, pDescricao, pRemuneracao);
+
+    } catch (erro) {
+        console.log(`Ocorreu alguma erro ao detalhar a vaga. (${erro})`);
+    }
+
+    irPara('list-jobs', 'jobs-description');
 }
